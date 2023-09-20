1e1a5c646f4321bc2cb10d2d707894b4df78148f
Alex Eftimie <65734304+alex-eftimie@users.noreply.github.com>
2023-09-20 19:29:36 +0300

    submodules fixes

--- src/Listeners/UpdateCache.php
+++ src/Listeners/UpdateCache.php
@@ -2,6 +2,7 @@
 
 namespace OptimistDigital\NovaRedirects\Listeners;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
 use OptimistDigital\NovaRedirects\Models\Redirect;
 
@@ -21,10 +22,14 @@ class UpdateCache
    */
   public function handle($event): void
   {
-    $redirects = Redirect::all();
-    $redirects = $redirects->mapWithKeys(function ($redirect) {
-      return [$redirect->from_url => [$redirect->to_url, $redirect->status_code]];
-    })->toArray();
-    Cache::forever('nova-redirects', $redirects);
+    try {
+      $redirects = Redirect::all();
+      $redirects = $redirects->mapWithKeys(function ($redirect) {
+        return [$redirect->from_url => [$redirect->to_url, $redirect->status_code]];
+      })->toArray();
+      Cache::forever('nova-redirects', $redirects);
+    } catch (\Exception $e) {
+      Log::error('UpdateCache', ['error' => $e->getMessage()]);
+    }
   }
 }
